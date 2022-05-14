cf17ddeb2df0af48c258b5be38a007ee5b0e7757
Cipher-OOS <ma7moudmo7med628@outlook.sa>
2022-05-15 00:31:34 +0200

    Finalizing and cleaning up
    Finishing SS94A2 Code and cleaning up the unused code

--- Source Code/SS94A2/Magneto_SS94A2.py
+++ Source Code/SS94A2/Magneto_SS94A2.py
@@ -1,40 +1,24 @@
 from machine import ADC, Pin      
 import time
 import datetime
-import remap
-
 adc = ADC(Pin(26))
 sensor_val=0
 PrevGauss=0
 cal=0
 reading = adc.read_u16
 VoltageRef=5
-ADC_Voltage = (reading * VoltageRef) / 1024         #converting analog value to digtal value from 0-1023 to 0-5
+ADC_Voltage = (reading * VoltageRef) / 4096         #converting analog value to digtal value from 0-1023 to 0-5
 Voltage = ADC_Voltage
 Cal = Voltage           #Calibration voltage
 
 while True :
-    ADC_Voltage = (reading * VoltageRef) / 1024.0
-    Voltage = ADC_Voltage
-    if Voltage >= cal :
-        Gauss = ((Voltage - Cal)/0.00033)
-
-
-
-
-
-
-
-def sensorCallback(channel):
+  ADC_Voltage = (reading * VoltageRef) / 1024.0
+  Voltage = ADC_Voltage
+  #Gauss = ((Voltage - Cal)/0.00033)    #Calculating EQN - Left unkown for consulting 
+  def sensorCallback(channel):
   # Called if sensor output changes
-  timestamp = time.time()
-  stamp = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M:%S')
-  if adc.read_u16(channel):
-   
-    print(adc.read_u16 Gauss)
-  else:
-    # Magnet
-    print("Sensor LOW " + stamp)
-
-
-
+    timestamp = time.time()
+    stamp = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M:%S')
+    if adc.read_u16(channel):
+         print(adc.read_u16 Gauss)  
+  time.sleep(1.5)
