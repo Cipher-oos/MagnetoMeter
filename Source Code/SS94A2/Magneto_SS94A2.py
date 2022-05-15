ace730544f2b79c942c22b4a6fd7ff8bf6cf9302
Cipher-OOS <ma7moudmo7med628@outlook.sa>
2022-05-16 01:10:00 +0200

    Adding support for relay board for SS94A2
    Adding relay board support for Pico to handle voltages higher than 3.3 volts

--- Source Code/SS94A2/Magneto_SS94A2.py
+++ Source Code/SS94A2/Magneto_SS94A2.py
@@ -1,6 +1,17 @@
 from machine import ADC, Pin      
 import time
 import datetime
+import utime
+
+
+#Define Relay Board for Highr Voltage than 3.3 Volt
+relay1 = Pin(2, Pin.OUT)
+relay2 = Pin(3, Pin.OUT)
+relay3 = Pin(4, Pin.OUT)
+relay4 = Pin(5, Pin.OUT)
+
+
+#Sensor Virables Definiation :
 adc = ADC(Pin(26))
 sensor_val=0
 PrevGauss=0
@@ -11,7 +22,24 @@ ADC_Voltage = (reading * VoltageRef) / 4096         #converting analog value to 
 Voltage = ADC_Voltage
 Cal = Voltage           #Calibration voltage
 
+
+
 while True :
+  #Relay Board :
+  relay1.toggle()
+    utime.sleep(0.5)
+    relay2.toggle()
+    utime.sleep(0.5)
+    relay3.toggle()
+    utime.sleep(0.5)
+    relay4.toggle()
+    utime.sleep(0.5)
+    relay1(1)
+    utime.sleep(0.5)
+    relay1(0)
+    utime.sleep(0.5)
+
+  #Sensor 
   ADC_Voltage = (reading * VoltageRef) / 1024.0
   Voltage = ADC_Voltage
   if Voltage != 0 :
